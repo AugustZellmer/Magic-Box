8ef7210ef708ab184711720c8ff35aafa0274767
August Zellmer <AugustZellmer@users.noreply.github.com>
2020-01-31 22:53:25 -0600

    got window rotation actually working correctly. surprising, I know.

--- Assets/Scripts/WindowMovement.cs
+++ Assets/Scripts/WindowMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class WindowMovement : MonoBehaviour
 {
 
+    Gyroscope gyro;
+
     void Start()
     {
         gyro = Input.gyro;
@@ -13,11 +15,15 @@ public class WindowMovement : MonoBehaviour
 
     void Update()
     {
-        transform.localRotation = GyroToUnity(gyro.attitude);
+        transform.localRotation = getAndroidGyroscopeRotation();
     }
 
-    private static Quaternion GyroToUnity(Quaternion q)
+    /*  
+    I don't completely understand how this method works, but by the time it's done it will return a rotation in which
+    north is positive x, up is positive y, and east is positive z, which is exactly what we want for unity's coordinate system.
+    */
+    private Quaternion getAndroidGyroscopeRotation()
     {
-        return new Quaternion(.5f, .5f, -.5f, .5f) * q;
+        return Quaternion.Euler(0, 90, 0) * (new Quaternion(.5f, -.5f, .5f, .5f) * gyro.attitude) * Quaternion.Euler(0, 0, 180); 
     }
 }
